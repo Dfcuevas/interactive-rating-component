3056f7d7c54968eeaa92092333adfa9c36db4cca
Diego Cuevas <ingdcuevas@gmail.com>
2022-04-16 20:24:58 -0500

    haciendo cambios al componente Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,6 +11,14 @@ function Main() {
         Please let us know how we did with your support request. All feedback is
         appreciated to help us improve our offering!
       </p>
+      <div className="rating-container">
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+      </div>
+      <button type="submit">SUBMIT</button>
     </main>
   );
 }
